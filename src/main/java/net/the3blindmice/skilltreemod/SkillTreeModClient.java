08bcd657527d24951c21c2002410ba4d23d67646
TomCha04 <tomaschaparroossa@gmail.com>
2025-01-11 15:30:01 -0500

    Initial commit

---
+++ src/main/java/net/the3blindmice/skilltreemod/SkillTreeModClient.java
@@ -0,0 +1,10 @@
+package net.the3blindmice.skilltreemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkillTreeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/the3blindmice/skilltreemod/SkillTreeModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.the3blindmice.skilltreemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkillTreeModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
